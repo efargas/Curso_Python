9c42196695af1388cbc45ec6f3c118e26d5e88e5
Erik Fargas <efargaspro@gmail.com>
2020-12-02 16:19:50 +0100

    Ejercicio Entrada y Salida

---
+++ 2-ConceptosBasicos/pimer_ejercicio.py
@@ -0,0 +1,5 @@
+print('Suma de dos números')
+num1 = int(input('Introduce el primer número\n'))
+num2 = int(input('Introduce el segundo número\n'))
+res = num1 + num2
+print(f'El resultado de sumar {num1} + {num2} = {res}')
